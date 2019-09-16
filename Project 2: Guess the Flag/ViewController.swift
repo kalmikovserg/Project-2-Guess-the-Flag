1ec1349a0b8b32be7507f0c302bb111b6fd587b2
Kalmikov Sergey <KalmikovW13@gmail.com>
2019-09-15 19:30:07 -0500

    Initial Commit

---
+++ Project 2: Guess the Flag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 2: Guess the Flag
+//
+//  Created by Сергей Калмыков on 9/15/19.
+//  Copyright © 2019 Сергей Калмыков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
